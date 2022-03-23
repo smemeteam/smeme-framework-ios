fa71df61f94468a494b554280f17a094d49282c6
Konstantin Sonntag <dev@moresparechange.com>
2022-03-23 19:28:38 +0100

    initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# smeme-framework-ios
---
+++ framework/framework.h
@@ -0,0 +1,18 @@
+//
+//  framework.h
+//  framework
+//
+//  Created by Konstantin Sonntag on 23.03.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for framework.
+FOUNDATION_EXPORT double frameworkVersionNumber;
+
+//! Project version string for framework.
+FOUNDATION_EXPORT const unsigned char frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <framework/PublicHeader.h>
+
+
